1c8b34cdd6aced16993f323367b224def19af1c7
Kenil Gondaliya <kenilgondaliya960@gmail.com>
2026-02-20 16:44:08 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Upload from "./artifacts/contracts/Upload.sol/Upload.json";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import FileUpload from "./components/FileUpload";
+import Display from "./components/display";
+import Modal from "./components/Modal";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [account, setAccount] = useState("");
+  const [contract, setContract] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+  const loadProvider = async () => {
+    if (window.ethereum) {
+      const provider = new ethers.BrowserProvider(window.ethereum);
+
+      await provider.send("eth_requestAccounts", []);
+
+      const signer = await provider.getSigner();
+      const address = await signer.getAddress();
+      setAccount(address);
+
+      const contractAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+
+      const contract = new ethers.Contract(
+        contractAddress,
+        Upload.abi,
+        signer
+      );
+
+      console.log(contract);
+
+      setContract(contract);
+      setProvider(provider);
+    } else {
+      console.error("Please install MetaMask!");
+    }
+  };
+
+  loadProvider();
+}, []);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Decentralized File System</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/FileUpload.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FileUpload() {
+  return (
+    <div>
+      <h1>File Upload Component</h1>
+    </div>
+  )
+}
